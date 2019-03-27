6f949fa1a6949a053bd8247b7115578ddc970da1
bulat <bulat.93@inbox.ru>
2019-03-27 22:02:00 +0300

    lombok refactor

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -2,6 +2,7 @@ package guru.springframework.controllers;
 
 import guru.springframework.repositories.RecipeRepository;
 import guru.springframework.service.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 /**
  * Created by jt on 6/1/17.
  */
+@Slf4j
 @Controller
 public class IndexController {
 
@@ -20,6 +22,8 @@ public class IndexController {
 
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage(Model model){
+        log.debug("Getting Index Page");
+
         model.addAttribute("recipes", recipeService.getRecipes());
 
         return "index";
